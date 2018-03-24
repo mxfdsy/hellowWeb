59cfd7b3e9814826133c3c1636f955bc5701ad37
平凡的世界 <963559210@qq.com>
2018-03-24 16:05:40 +0800

    cookies 和session 加入

--- src/ServletDemo1.java
+++ src/ServletDemo1.java
@@ -20,14 +20,17 @@ public class ServletDemo1 extends HttpServlet {
 //        String pwd = new String(request.getParameter("pwdP").getBytes("ISO_8859_1"),"UTF-8");
         String name =request.getParameter("nameP");
         String pwd = request.getParameter("pwdP");
+        //把请求传到JSP文件
+        request.setAttribute("name",name);
+        request.getRequestDispatcher("/WEB-INF/HellowJsp.jsp").forward(request,response);
 //        String sessionValue = (String)request.getSession().getAttribute("xxx");
 //        System.out.println(sessionValue);
         System.out.println(name + "————————— "+ pwd);
         //cookies
-        Cookie cookie = new Cookie("CooklesName",name);
-        cookie.setMaxAge(60*60*24);
-        response.addCookie(cookie);
-        System.out.println(request.getSession().getId());
+//        Cookie cookie = new Cookie("CooklesName",name);
+//        cookie.setMaxAge(60*60*24);
+//        response.addCookie(cookie);
+//        System.out.println(request.getSession().getId());
         //获取请求中的cookes数据
 //        Cookie[] cookies = request.getCookies();
 //        for (int i = 0; i <cookies.length ; i++) {
@@ -60,20 +63,20 @@ public class ServletDemo1 extends HttpServlet {
 
         //response 简单请求的处理
 //        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        try {
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>给你的回应</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h2> Hello   " + name + "，欢迎您</h2>");
-            out.println("<h2> 现在是 </h2>" + new Date().toLocaleString());
-            out.println("</body>");
-            out.println("</html>");
-        }finally {
-            out.close();
-        }
+//        PrintWriter out = response.getWriter();
+//        try {
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>给你的回应</title>");
+//            out.println("</head>");
+//            out.println("<body>");
+//            out.println("<h2> Hello   " + name + "，欢迎您</h2>");
+//            out.println("<h2> 现在是 </h2>" + new Date().toLocaleString());
+//            out.println("</body>");
+//            out.println("</html>");
+//        }finally {
+//            out.close();
+//        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
